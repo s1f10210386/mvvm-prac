cb1040b0febb8f9354999afebd5f693a244536f8
hotaka <s1f102103864@iniad.org>
2024-02-05 01:55:50 +0900

    initial

---
+++ mvvm-prac/ListRow.swift
@@ -0,0 +1,18 @@
+//
+//  ListRow.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRow()
+}
