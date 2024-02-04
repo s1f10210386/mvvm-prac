a327a35d1ecf300b5f5f4fcf9c0afae5de3cc9d2
hotaka <s1f102103864@iniad.org>
2024-02-05 01:12:21 +0900

    Initial Commit

---
+++ mvvm-prac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
