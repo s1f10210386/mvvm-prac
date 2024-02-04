c550bd7b21f8d7701368aa71269b4bb2edbf6921
hotaka <s1f102103864@iniad.org>
2024-02-05 03:07:47 +0900

    feat: ok

---
+++ mvvm-prac/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserData()
+}
---
+++ mvvm-prac/UserTask.swift
@@ -0,0 +1,18 @@
+//
+//  UserTask.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct UserTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserTask()
+}
