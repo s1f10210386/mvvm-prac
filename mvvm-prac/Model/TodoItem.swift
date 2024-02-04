e032966490cf9d4d6b294060cf20429de5b2f93e
hotaka <s1f102103864@iniad.org>
2024-02-05 01:12:21 +0900

    Initial Commit

---
+++ mvvm-prac/Model/TodoItem.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItem.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+struct TodoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoItem()
+}
---
+++ mvvm-prac/mvvm_pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_pracApp.swift
+//  mvvm-prac
+//
+//  Created by 金澤帆高 on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
